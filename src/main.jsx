3536d44c065d43f5704f4e63b3df47c31cb68adf
carvalhosete <lcarvalho7@outlook.com>
2025-09-08 14:07:21 -0300

    Implementado: melhora no uso do JWT || integração com APi do google para login.

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,9 @@ import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
 import { BrowserRouter } from 'react-router-dom';
 import './pages/styles/index.css';
+import axios from 'axios';
+
+axios.defaults.withCredentials = true;
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
--- src/pages/components/ProtectedRoute.jsx
+++ src/pages/components/ProtectedRoute.jsx
@@ -1,15 +1,15 @@
-import { Navigate } from "react-router-dom";
+    import { Navigate } from "react-router-dom";
 
-function ProtectedRoute({ children }) {
-    const token = localStorage.getItem('token');
+    function ProtectedRoute({ children }) {
+        const token = localStorage.getItem('token');
 
-    //se não tiver token redirecione para página de login.
-    if (!token) {
-        return <Navigate to="/login" />;
+        //se não tiver token redirecione para página de login.
+        if (!token) {
+            return <Navigate to="/login" />;
+        }
+
+        //se houver touken renderiza o componente children que foi passado (DashboardPage)
+        return children;
     }
 
-    //se houver touken renderiza o componente children que foi passado (DashboardPage)
-    return children;
-}
-
-export default ProtectedRoute;
+    export default ProtectedRoute;
